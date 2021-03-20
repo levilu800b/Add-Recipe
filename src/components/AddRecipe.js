b3a462b851f88708e740ffaeeafcab69262ba54f
levilu800b <levilu800b@gmail.com>
2021-03-20 20:58:52 +0000

    change

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {Container, Row, Col, Button} from 'react-bootstrap';
-import {FaPlus} from 'react-icons/fa';
+import {Container, Row, Col} from 'react-bootstrap';
+import {FaPlus} from 'react-icons/fa'
 
  class AddRecipe extends React.Component {
-    state = {
+    constructor(props) {
+        super(props);
+        this.state = {
         recipetitle: "",
         preptime: "",
         cooktime: "",
         serves: "",
-        measurement: [{measurement:""}],
-        unit:  [{unit:""}],
-        ingredient:  [{ingredient:""}],
-        steps: [{steps:""  }],
-        number: [{number:""}],
+        measurement: "",
+        unit: "",
+        ingredients: [{measurement:"", unit:"", ingredients:""}],
       };
+    }
     
       submitHandler = event => {
         event.preventDefault();
@@ -24,17 +25,13 @@ import {FaPlus} from 'react-icons/fa';
         this.setState({ [event.target.name]: event.target.value });
       };
 
-      handleIngredients (newData, index)=>{
-        this.setState(state=> {
-        Const newarray = state.ingredients.splice(index, 1, Data)
-        Return {ingredients: […newarray]}
-        }
-        )
+      
     
     render() {
         return (
             <Container>
         <div className="Form">
+          <h1 className="header1">Create A Recipe</h1>
           <form
             className="needs-validation"
             onSubmit={this.submitHandler}
@@ -68,20 +65,20 @@ import {FaPlus} from 'react-icons/fa';
               </label>
                 <select
                   onChange={this.changeHandler}
-                  type="select"
+                  type="numberofguests"
                   id="defaultFormRegisterConfirmEx3"
                   className="form-control"
                   name="preptime"
                   placeholder="Prep Time">
                   <option value="">Prep Time</option>
-                  <option value="5 minutes">5 minutes</option>
-                  <option value="10 minutes">10 minutes</option>
-                  <option value="15 minutes">15 minutes</option>
-                  <option value="20 minutes">20 minutes</option>
-                  <option value="25 minutes">25 minutes</option>
-                  <option value="30 minutes">30 minutes</option>
-                  <option value="35 minutes">35 minutes</option>
-                  <option value="40 minutes">40 minutes</option>
+                  <option value="1">5 minutes</option>
+                  <option value="2">10 minutes</option>
+                  <option value="3">15 minutes</option>
+                  <option value="4">20 minutes</option>
+                  <option value="5">25 minutes</option>
+                  <option value="6">30 minutes</option>
+                  <option value="7">35 minutes</option>
+                  <option value="8">40 minutes</option>
                 </select>
                   </Col>
                   <Col>
@@ -92,20 +89,20 @@ import {FaPlus} from 'react-icons/fa';
               </label>
                 <select
                   onChange={this.changeHandler}
-                  type="select"
+                  type="numberofguests"
                   id="defaultFormRegisterConfirmEx3"
                   className="form-control"
                   name="cooktime"
                   placeholder="Cook Time">
                   <option value="">Cook Time</option>
-                  <option value="10 minutes">10 minutes</option>
-                  <option value="20 minutes">20 minutes</option>
-                  <option value="30 minutes">30 minutes</option>
-                  <option value="40 minutes">40 minutes</option>
-                  <option value="50 minutes">50 minutes</option>
-                  <option value="1 Hour">1 Hour</option>
-                  <option value="1.30 Hours">1.30 Hours</option>
-                  <option value="2 Hours">2 Hours</option>
+                  <option value="1">10 minutes</option>
+                  <option value="2">20 minutes</option>
+                  <option value="3">30 minutes</option>
+                  <option value="4">40 minutes</option>
+                  <option value="5">50 minutes</option>
+                  <option value="6">1 Hour</option>
+                  <option value="7">1.30 Hours</option>
+                  <option value="8">2 Hours</option>
                 </select>
                   </Col>
                   <Col>
@@ -116,7 +113,7 @@ import {FaPlus} from 'react-icons/fa';
               </label>
                 <select
                   onChange={this.changeHandler}
-                  type="select"
+                  type="numberofguests"
                   id="defaultFormRegisterConfirmEx3"
                   className="form-control"
                   name="serves"
@@ -133,24 +130,23 @@ import {FaPlus} from 'react-icons/fa';
                 </select>
                   </Col>
               </Row>
-              <h2 className="header1">Add Ingredients</h2>
+              <h2 className="header2">Add Ingredients</h2>
               <br />
               <Row>
-               <Button className="button-plus" onClick={()=>{
-                  this.setState(previousState=>({measurement:[...previousState.measurement,
-                  {measurement:"", unit:"", ingredients:""}]}))}}>
-              <FaPlus />
-              </Button>
-                  <Col>
+              <FaPlus className="button-plus" onClick={()=>{
+                  this.setState(previousState=>({ingredients:[...previousState.ingredients,
+                  {measurement:"", unit:"", ingredients:""}]}))}}/>
+              <Col>
                   <label
                   htmlFor="defaultFormRegisterNameEx"
                   className="grey-text"
                 >
               </label>
-              {this.state.measurement.map((measurement, index)=> (
-               <input type="text"
-               onChange={(event)=>this.handleIngredient(event.target.value, index)}
-                key={measurement.name}
+              {this.state.ingredients.map(ingredients=>(
+                <input type="text"
+                  value={this.state.measurement}
+                  name="measurement"
+                  onChange={this.changeHandler}
                   id="defaultFormRegisterNameEx"
                   className="form-control"
                   placeholder="Measurement"
@@ -158,33 +154,30 @@ import {FaPlus} from 'react-icons/fa';
                 />
               ))}
                 <div className="valid-feedback">Looks good!</div>
-                  </Col>
+                </Col>
                   <Col>
                   <label
                   htmlFor="defaultFormRegisterConfirmEx3"
                   className="grey-text"
                 >
               </label>
-              {this.state.measurement.map(unit=> (
                 <select
                   onChange={this.changeHandler}
-                  key={unit.name}
-                  type="select"
+                  type="numberofguests"
                   id="defaultFormRegisterConfirmEx3"
                   className="form-control"
                   name="unit"
                   placeholder="Unit">
                   <option value="">Unit</option>
-                  <option value="c(Cup)">c(Cup)</option>
-                  <option value="l(Liter)">l(Liter)</option>
-                  <option value="kg(Kilogram)">kg(Kilogram)</option>
-                  <option value="g(Gram)">g(Gram)</option>
-                  <option value="tsp(Teaspoon)">tsp(Teaspoon)</option>
-                  <option value="dp(Drop)">dp(Drop)</option>
-                  <option value="oz(Ounce)">oz(Ounce)</option>
-                  <option value="Tbsp(Tablespoon)">Tbsp(Tablespoon)</option>
+                  <option value="1">c(Cup)</option>
+                  <option value="2">l(Liter)</option>
+                  <option value="3">kg(Kilogram)</option>
+                  <option value="4">g(Gram)</option>
+                  <option value="5">tsp(Teaspoon)</option>
+                  <option value="6">dp(Drop)</option>
+                  <option value="7">oz(Ounce)</option>
+                  <option value="8">Tbsp(Tablespoon)</option>
                 </select>
-              ))}
                   </Col>
                   <Col>
                   <label
@@ -192,68 +185,21 @@ import {FaPlus} from 'react-icons/fa';
                   className="grey-text"
                 >
               </label>
-              {this.state.measurement.map(ingredient=> (
                 <input type="text"
-                key={ingredient.name}
-                  name="ingredient"
                   onChange={this.changeHandler}
                   id="defaultFormRegisterNameEx"
                   className="form-control"
-                  placeholder="Ingredient"
+                  placeholder="Ingredients"
                   required
                 />
-              ))}
                 <div className="valid-feedback">Looks good!</div>
+                </Col>
+              </Row>
+              <h2 className="header2">Add Steps</h2>
+              <Row>
+                  <Col>
                   </Col>
               </Row>
-              <h2 className="header1">Add Steps</h2>
-              <Row>
-              <Button className="button-plus" onClick={()=>{
-                        this.setState(previousState=>({steps:[...previousState.steps,
-                         {steps:"", number:previousState.currentStep}],
-                          currentStep: previousState.currentStep++}))}}>
-              <FaPlus />
-              </Button>
-              <Col xs={2}>
-              <label
-                  htmlFor="defaultFormRegisterNameEx"
-                  className="grey-text"
-                >
-              </label>
-              {this.state.steps.map(number=> (
-                <input 
-                key={number.name}
-                  onChange={this.changeHandler}
-                  id="defaultFormRegisterNameEx"
-                  className="form-control"
-                  placeholder="1"
-                  required
-                />
-              ))}
-                <div className="valid-feedback">Looks good!</div>
-              </Col>
-                  <Col>
-                <label
-                  htmlFor="defaultFormRegisterNameEx"
-                  className="grey-text"
-                >
-              </label>
-              {this.state.steps.map(steps=> (
-                <input type="text"
-                key={steps.name}
-                  onChange={this.changeHandler}
-                  id="defaultFormRegisterNameEx"
-                  className="form-control"
-                  placeholder="Step One"
-                  required
-                />
-              ))}
-                <div className="valid-feedback">Looks good!</div>
-              </Col>
-              </Row>
-              <Button className="button-submit" color="primary" type="submit">
-                  Create Recipe
-              </Button>
               </form>
               </div>
               </Container>
